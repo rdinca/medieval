ea28a77c449a0c64a23159db2311c5b8e1b38ba3
rdinca <radudinca@yahoo.com>
2010-06-14 12:10:16 +0300

    battalion.rb

---
+++ lib/medieval/battalion.rb
@@ -0,0 +1,26 @@
+module Medieval
+  class Battalion
+    class << self
+      def creature_name
+        %w(Dragons Cyclops Wyverns Studs Dwarfs).rand
+      end
+      
+      def ability
+        %w(Brave Murderous Poor Crazy Great Fast).rand
+      end
+      
+      def ability2
+       
+      end
+
+
+
+    end  
+    
+    FORMATS = [
+    
+    ]
+    
+    
+  end
+end
