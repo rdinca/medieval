f616f61f0a9c2cb0a56cfc5643afb69e843864fb
rdinca <radudinca@yahoo.com>
2010-12-20 18:03:15 +0200

    minor changes in order to fit my plans

--- lib/medieval/battalion.rb
+++ lib/medieval/battalion.rb
@@ -20,11 +20,12 @@ protected
     FORMATS = [
       Proc.new { ["The", name_of_order, formation_name, "of", ability, colour, creature_name]}, 
       Proc.new { ["The", name_of_order, formation_name, "of", ability, colour, creature_name]}, 
-      Proc.new { ["The", ability, colour, creature_name]},       
       Proc.new { ["The", name_of_order, formation_name, "of", ability, creature_name]}, 
-      Proc.new { ["The", name_of_order, formation_name, "of", colour, creature_name]},       
-      Proc.new { ["The", colour, creature_name]},
-      Proc.new { ["The", ability , creature_name]}    
+      Proc.new { ["The", name_of_order, formation_name, "of", colour, creature_name]},  
+      #Proc.new { ["The", ability, colour, creature_name]},                  
+      #Proc.new { ["The", colour, creature_name]},
+      #Proc.new { ["The", ability , creature_name]}    
+      # removed some less interesting formats 
     ]
 
   end
--- lib/medieval/realm.rb
+++ lib/medieval/realm.rb
@@ -1,27 +1,38 @@
 module Medieval  
   class Realm
     extend Attributes
+    
     class << self
       def name
-        FORMATS.rand.call.join('')
+        FORMATS.rand.call.join(' ')
       end
  
-protected     
-      alias main_name creature_name
-      
+    protected     
+      def main_name
+        creature_name + realm_suffix
+      end
+
+=begin       
       def suffix
         %w(oin yar oyd aire atya uti aro yal iha).rand
       end 
+=end      
+      def realm_prefix
+        %w(North East West South New Lake Port Far Lost).rand
+      end      
       
-      def land_name
-        %w(Land County Shire Realm).rand
+      def realm_suffix
+        %w(county realm ton land ville berg burgh borough bury view port mouth stad furt chester mouth fort haven side shire).rand      
       end    
+      
       
     end
     
     FORMATS = [
-      Proc.new {[ability, main_name, land_name]},
-      Proc.new {[main_name, land_name]}          
+#      Proc.new {[ability, main_name, realm_suffix]},
+      Proc.new {[main_name]},
+#      Proc.new {[realm_prefix, ' ' , ability, main_name, realm_suffix]},
+      Proc.new {[realm_prefix, main_name]}
     ]
 end
   
