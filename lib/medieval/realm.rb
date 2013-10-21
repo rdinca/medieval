7516234c747bc706bb2dcb1c285e706073c03839
Radu Dinca-Stefan <rdinca@Artsoft56.(none)>
2013-10-21 11:17:19 +0300

    ubber minor

--- lib/medieval/realm.rb
+++ lib/medieval/realm.rb
@@ -22,7 +22,7 @@ module Medieval
       end
 
       def realm_suffix
-        %w(county realm ton land ville berg burgh borough bury view port mouth stad furt chester mouth fort haven side shire).rand
+        %w(county realm ton land ville berg burgh borough bury view port mouth stad furt chester mouth fort haven side shire  sway).rand
       end
 
 
